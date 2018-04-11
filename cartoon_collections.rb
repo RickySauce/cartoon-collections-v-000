06df8d1d87e284626fc1aab390b5fb66fa87f1c0
Eric DeSimone <eric.desimone@cortland.edu>
2018-04-11 13:54:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
 def roll_call_dwarves(array)# code an argument here
-  # Your code here
+ do array.each_with_index |dwarves, index|
+   index += 1
+   puts "#{index} #{dwarves}"
 end
 
 def summon_captain_planet# code an argument here
